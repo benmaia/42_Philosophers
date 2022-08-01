eba331b208dedba62c620828bac132d3d619a1d3
benmaia <godlinhomaia@gmail.com>
2022-08-01 03:20:01 +0100

    init the project

---
+++ philosophers/inc/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmiguel- <bmiguel-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 02:59:52 by bmiguel-          #+#    #+#             */
+/*   Updated: 2022/08/01 03:17:42 by bmiguel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <pthread.h>
+# include <unistd.h>
+
+
+#endif
