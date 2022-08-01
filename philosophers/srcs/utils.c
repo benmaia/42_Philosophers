bc9d6fb8f455fc0d29fb9c21228b74ae3b3517df
benmaia <godlinhomaia@gmail.com>
2022-08-01 15:20:35 +0100

    add free ft

--- philosophers/srcs/utils.c
+++ philosophers/srcs/utils.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmiguel- <bmiguel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/01 02:59:52 by bmiguel-          #+#    #+#             */
-/*   Updated: 2022/08/01 15:18:59 by bmiguel-         ###   ########.fr       */
+/*   Created: 2022/08/01 15:18:21 by bmiguel-          #+#    #+#             */
+/*   Updated: 2022/08/01 15:18:41 by bmiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "../inc/philo.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <pthread.h>
-# include <unistd.h>
-
-void	ft_free(void **ptr);
-
-#endif
+void	ft_free(void **ptr)
+{
+	if (*ptr)
+		free (*ptr);
+	ptr = NULL;
+}
